2273559cb608b82ea60a6ec4f36565348e03bf40
João Moura <joao87moura@gmail.com>
2014-02-26 18:02:38 +0000

    Socialshare

--- jayScript.js
+++ jayScript.js
@@ -399,51 +399,7 @@ var j = {
 		xmlhttp.open("GET", target, true);
 		xmlhttp.send();
 	},
-    publish: function (params) {
-        "use strict";
-        var link, windowParams;
-        if (params.socialTarget === "facebook") {
-            link = "https://www.facebook.com/sharer/sharer.php?s=100&p[url]=" + params.url + "&p[images][0]=" + encodeURIComponent(params.image) + "&p[title]=" + encodeURIComponent(params.title) + "&p[summary]=" + encodeURIComponent(this.strip(params.summary));
-        } else if (params.socialTarget === "twitter") {
-            link = "http://twitter.com/share?text=" + encodeURIComponent(this.strip(params.summary)) + "&url=" + params.url;
-        } else if (params.socialTarget === "linkedin") {
-            link = "http://www.linkedin.com/shareArticle?mini=true&url=" + params.url + "&title=" + params.title + "&summary=" + this.strip(params.summary);
-        } else if (params.socialTarget === "googleplus") {
-            link = "https://plus.google.com/share?url=" + params.url;
-        } else if (params.socialTarget === "googlebookmark") {
-            link = "http://www.google.com/bookmarks/mark?op=edit&bkmk=" + params.url + "&title=" + params.title + "&annotation=" + params.summary;
-        } else if (params.socialTarget === "pinterest") {
-            link = "http://pinterest.com/pin/create/button/?url=" + params.url + "&media=" + encodeURIComponent(params.image) + "&description=" + params.summary;
-        } else if (params.socialTarget === "tumblr") {
-            if (params.sharetype === "photo") {
-                link = "http://www.tumblr.com/share/photo?source=" + encodeURIComponent(params.image) + "&caption=" + params.summary + "&click_thru=" + encodeURIComponent(params.url);
-            } else {
-                link = "http://www.tumblr.com/share/link?url=" + encodeURIComponent(params.url) + "&name=" + params.title + "&description=" + params.summary;
-            }
-        } else if (params.socialTarget === "delicious") {
-            link = "http://delicious.com/post?url=" + params.url + "&title=" + params.title + "&notes=" + params.summary;
-        } else if (params.socialTarget === "reddit") {
-            link = "http://www.reddit.com/submit?url=" + params.url + "&title=" + params.title;
-        } else if (params.socialTarget === "tapiture") {
-            link = "http://tapiture.com/bookmarklet/image?img_src=" + params.image + "&page_url=" + params.url + "&page_title=" + params.title + "&img_title=" + params.imgTitle + "&img_width=" + params.width + "img_height=" + params.height;
-        } else if (params.socialTarget === "stumbleupon") {
-            link = "http://www.stumbleupon.com/submit?url=" + params.url + "&title=" + params.title;
-        } else if (params.socialTarget === "newsvine") {
-            link = "http://www.newsvine.com/_tools/seed&save?u=" + params.url + "&h=" + params.title;
-        }
-        if (link !== "") {
-            windowParams = "";
-            if (params.socialTarget === "tumblr") {
-                windowParams = "toolbar=0,status=0,width=800,height=500";
-            } else if (params.socialTarget === "googleplus") {
-                windowParams = "toolbar=0,status=0,width=600,height=600";
-
-            } else {
-                windowParams = "toolbar=0,status=0,width=626,height=436";
-            }
-            window.open(link, 'sharer', windowParams);
-        }
-    },
+    
     share: function (element) {
         "use strict";
         var params = {};
@@ -456,7 +412,7 @@ var j = {
         params.width = element.getAttribute("data-image-width");
         params.height = element.getAttribute("data-image-height");
         params.socialTarget = element.getAttribute("data-type");
-        this.publish(params);
+        this.socialShare.publish(params);
         
     },
     
@@ -468,6 +424,51 @@ var j = {
     },
     
     socialShare: {
+        publish: function (params) {
+            "use strict";
+            var link, windowParams;
+            if (params.socialTarget === "facebook") {
+                link = "https://www.facebook.com/sharer/sharer.php?s=100&p[url]=" + params.url + "&p[images][0]=" + encodeURIComponent(params.image) + "&p[title]=" + encodeURIComponent(params.title) + "&p[summary]=" + encodeURIComponent(this.strip(params.summary));
+            } else if (params.socialTarget === "twitter") {
+                link = "http://twitter.com/share?text=" + encodeURIComponent(this.strip(params.summary)) + "&url=" + params.url;
+            } else if (params.socialTarget === "linkedin") {
+                link = "http://www.linkedin.com/shareArticle?mini=true&url=" + params.url + "&title=" + params.title + "&summary=" + this.strip(params.summary);
+            } else if (params.socialTarget === "googleplus") {
+                link = "https://plus.google.com/share?url=" + params.url;
+            } else if (params.socialTarget === "googlebookmark") {
+                link = "http://www.google.com/bookmarks/mark?op=edit&bkmk=" + params.url + "&title=" + params.title + "&annotation=" + params.summary;
+            } else if (params.socialTarget === "pinterest") {
+                link = "http://pinterest.com/pin/create/button/?url=" + params.url + "&media=" + encodeURIComponent(params.image) + "&description=" + params.summary;
+            } else if (params.socialTarget === "tumblr") {
+                if (params.sharetype === "photo") {
+                    link = "http://www.tumblr.com/share/photo?source=" + encodeURIComponent(params.image) + "&caption=" + params.summary + "&click_thru=" + encodeURIComponent(params.url);
+                } else {
+                    link = "http://www.tumblr.com/share/link?url=" + encodeURIComponent(params.url) + "&name=" + params.title + "&description=" + params.summary;
+                }
+            } else if (params.socialTarget === "delicious") {
+                link = "http://delicious.com/post?url=" + params.url + "&title=" + params.title + "&notes=" + params.summary;
+            } else if (params.socialTarget === "reddit") {
+                link = "http://www.reddit.com/submit?url=" + params.url + "&title=" + params.title;
+            } else if (params.socialTarget === "tapiture") {
+                link = "http://tapiture.com/bookmarklet/image?img_src=" + params.image + "&page_url=" + params.url + "&page_title=" + params.title + "&img_title=" + params.imgTitle + "&img_width=" + params.width + "img_height=" + params.height;
+            } else if (params.socialTarget === "stumbleupon") {
+                link = "http://www.stumbleupon.com/submit?url=" + params.url + "&title=" + params.title;
+            } else if (params.socialTarget === "newsvine") {
+                link = "http://www.newsvine.com/_tools/seed&save?u=" + params.url + "&h=" + params.title;
+            }
+            if (link !== "") {
+                windowParams = "";
+                if (params.socialTarget === "tumblr") {
+                    windowParams = "toolbar=0,status=0,width=800,height=500";
+                } else if (params.socialTarget === "googleplus") {
+                    windowParams = "toolbar=0,status=0,width=600,height=600";
+    
+                } else {
+                    windowParams = "toolbar=0,status=0,width=626,height=436";
+                }
+                window.open(link, 'sharer', windowParams);
+            }
+        },
         setTheme: function (targetElements, path, theme, invertTheme) {
             "use strict";
             var curronready,
