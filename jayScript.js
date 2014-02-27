b60b7b016f611364bff5eae743e244131d9b7785
João Moura <joao87moura@gmail.com>
2014-02-27 15:32:27 +0000

    strict mode

--- jayScript.js
+++ jayScript.js
@@ -1,8 +1,9 @@
 /*jslint browser:true */
+/*jslint node: true */
+"use strict";
 var j = {
     moveFloatingBox : -1,
 	loadFile: function (items, fn) {
-        "use strict";
         function loadJs(fileName) {
             var script;
             script = document.createElement('script');
@@ -45,7 +46,6 @@ var j = {
 		}
 	},
 	checkIfFileLoaded: function (fileName, fileType) {
-        "use strict";
         var elems = "", i;
 		if (fileType === "js") {
 			elems = document.selectByTag('script');
@@ -65,8 +65,7 @@ var j = {
 		return true;
 	},
 	onPageLoaded: function (fn) {
-        "use strict";
-		if (window.addEventListener) {
+        if (window.addEventListener) {
 		    window.addEventListener('load', function () { fn(); }, false);
 		} else if (window.attachEvent) {
 			window.attachEvent('onload', function () { fn(); });
@@ -84,8 +83,7 @@ var j = {
 		}
 	},
 	onDomReady: function (fn) {
-        "use strict";
-		if (document.addEventListener) {
+        if (document.addEventListener) {
 			document.addEventListener('DOMContentLoaded', function () { fn(); }, false);
 		} else {
 			if (document.onreadystatechange) {
@@ -108,7 +106,6 @@ var j = {
 	},
 
     setFullBackground : function (link, target) {
-        "use strict";
         var element;
         target = typeof target !== 'undefined' ? target : "html";
         if (this.nameType(target) === 0) {
@@ -126,7 +123,6 @@ var j = {
     },
 
     select: function (element, scope) {
-        "use strict";
         if (this.nameType(element) === 0) {
             element = this.selectById(element.substring(1), scope);
         } else if (this.nameType(element) === 1) {
@@ -138,7 +134,6 @@ var j = {
     },
 
     selectById: function (element, scope) {
-        "use strict";
         if (scope === undefined) {
             return document.getElementById(element);
         } else {
@@ -147,7 +142,6 @@ var j = {
     },
 
     selectByClass: function (element, scope) {
-        "use strict";
         if (scope === undefined) {
             scope = document;
         }
@@ -161,7 +155,6 @@ var j = {
     },
 
     selectByTag: function (element, scope) {
-        "use strict";
         if (scope === undefined) {
             return document.getElementsByTagName(element);
         } else {
@@ -170,7 +163,6 @@ var j = {
     },
 
     nameType: function (element) {
-        "use strict";
         if (element.charAt(0) === '#') {
             return 0;
         } else if (element.charAt(0) === '.') {
@@ -181,7 +173,6 @@ var j = {
     },
 	
 	first: function (element) {
-        "use strict";
         if (element.length >= 1) {
             element = element[0];
         }
@@ -189,7 +180,6 @@ var j = {
 	},
     
     addClass: function (className, element) {
-        "use strict";
         var i, elemI;
         if (Object.prototype.toString.call(element) === '[object Array]') {
             for (elemI = 0; elemI < element.length; elemI = elemI + 1) {
@@ -212,7 +202,6 @@ var j = {
     },
     
     removeClass: function (className, element) {
-        "use strict";
         var i, elemI;
         if (Object.prototype.toString.call(element) === '[object Array]') {
             for (elemI = 0; elemI < element.length; elemI = elemI + 1) {
@@ -232,13 +221,11 @@ var j = {
     },
     
     hasClass: function (className, element) {
-        "use strict";
         return element.className.indexOf(className) > -1;
     },
 
     createFloatingBox : function (element) {
-        "use strict";
-		var stopMove = false,
+        var stopMove = false,
             topBar = document.createElement("div"),
             self = this,
             leftTopBar,
@@ -312,7 +299,6 @@ var j = {
     },
 	
 	showFloatingBox: function (element) {
-        "use strict";
         var zIndex;
 		this.selectByClass("floatingBoxBar").forEach(function (entry) {
 			zIndex = entry.parentNode.style.zIndex;
@@ -327,8 +313,6 @@ var j = {
 	},
     
     getQueryParams: function () {
-        "use strict";
-        
         /*jslint regexp:true */
         var qs = document.location.search.split("+").join(" "),
             params = {},
@@ -343,7 +327,6 @@ var j = {
     },
     
     throttle: function (fn, threshold, scope) {
-        "use strict";
         if (!threshold) {
             threshold = 250;
         }
@@ -371,7 +354,6 @@ var j = {
     },
     
     debounce: function (func, wait, immediate) {
-        "use strict";
         var timeout;
         return function () {
             var context = this, args = arguments;
@@ -389,7 +371,6 @@ var j = {
     },
 	
 	loadGoogleFont: function (fonts) {
-        "use strict";
         var protocol, key;
 		protocol = ('https:' === document.location.protocol ? 'https' : 'http');
         for (key in fonts) {
@@ -401,7 +382,6 @@ var j = {
 	},
 	
 	isMobile: function () {
-        "use strict";
         var agent;
 		agent = navigator.userAgent || navigator.vendor || window.opera;
 		if (/(android|bb\d+|meego)[\w\W]+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile[\w\W]+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(agent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(agent.substr(0, 4))) {
@@ -412,8 +392,7 @@ var j = {
 	},
 
 	get: function (target, callback) {
-        "use strict";
-		var xmlhttp;
+        var xmlhttp;
 		xmlhttp = new XMLHttpRequest();
 		if (callback) {
 			xmlhttp.onreadystatechange = function () {
@@ -428,14 +407,12 @@ var j = {
 	},
     
     strip: function (html) {
-        "use strict";
         var tmp = document.createElement("DIV");
         tmp.innerHTML = html.replace("\\n", " ");
         return tmp.textContent || tmp.innerText || "";
     },
     
     share: function (element) {
-        "use strict";
         var params = {};
         params.url = element.getAttribute("data-url");
         params.image = element.getAttribute("data-image");
@@ -452,7 +429,6 @@ var j = {
     
     socialShare: {
         publish: function (params) {
-            "use strict";
             var link, windowParams;
             if (params.socialTarget === "facebook") {
                 link = "https://www.facebook.com/sharer/sharer.php?s=100&p[url]=" + params.url + "&p[images][0]=" + encodeURIComponent(params.image) + "&p[title]=" + encodeURIComponent(params.title) + "&p[summary]=" + encodeURIComponent(j.strip(params.summary));
@@ -497,7 +473,6 @@ var j = {
             }
         },
         setTheme: function (targetElements, path, theme, invertTheme) {
-            "use strict";
             var curronready,
                 newonready;
             function imagesManager() {
