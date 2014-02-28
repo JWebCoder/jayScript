3895f25aed312085f7f76e253eca705e1da3c750
João Moura <joao87moura@gmail.com>
2014-02-28 14:22:49 +0000

    addEvent beta version

--- jayScript.js
+++ jayScript.js
@@ -104,7 +104,16 @@ var j = {
 			}
 		}
 	},
-
+    addEvent :function (html_element, event_name, event_function) {       
+        if (html_element.attachEvent) {
+            html_element.attachEvent("on" + event_name, function() {
+                event_function.call(html_element);
+            });
+        } else if (html_element.addEventListener) {
+            html_element.addEventListener(event_name, event_function, false);
+        }
+    },
+    
     setFullBackground : function (link, target) {
         var element;
         target = typeof target !== 'undefined' ? target : "html";
